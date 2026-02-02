0593949cc057644157a3bc7477feef37fbc0db77
thomas kim <kimthomas109@gmail.com>
2026-02-01 22:22:31 -0800

    [emacs] add typescript mode package

--- .emacs.custom.el
+++ .emacs.custom.el
@@ -9,7 +9,7 @@
      default))
  '(display-line-numbers-type 'relative)
  '(inhibit-startup-screen t)
- '(package-selected-packages '(gruber-darker-theme multiple-cursors)))
+ '(package-selected-packages '(gruber-darker-theme multiple-cursors typescript-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
